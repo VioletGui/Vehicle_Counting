c5d8aa55f594d9a9b9377849a84a851462647bb2
YUE GUI <guiyviolet@gmail.com>
2017-03-21 23:06:03 -0400

    Add files via upload

---
+++ AlexNet_tflearn.py
@@ -0,0 +1,43 @@
+import tflearn
+from tflearn.layers.core import input_data, dropout, fully_connected
+from tflearn.layers.conv import conv_2d, max_pool_2d
+from tflearn.layers.normalization import local_response_normalization
+from tflearn.layers.estimator import regression
+
+height = 402
+width = 539
+max_num = 5
+
+from tflearn.data_utils import image_preloader
+X, Y = image_preloader(
+    '/media/violet/New Volume/dell/Documents/Files/学习/courses/CS/771/videos/people/people_train',
+    image_shape=(height, width),   mode='folder', categorical_labels=True,   normalize=True)
+
+# Building 'AlexNet'
+network = input_data(shape=[None, width, height, 3])
+network = conv_2d(network, 96, 11, strides=4, activation='relu')
+network = max_pool_2d(network, 3, strides=2)
+network = local_response_normalization(network)
+network = conv_2d(network, 256, 5, activation='relu')
+network = max_pool_2d(network, 3, strides=2)
+network = local_response_normalization(network)
+network = conv_2d(network, 384, 3, activation='relu')
+network = conv_2d(network, 384, 3, activation='relu')
+network = conv_2d(network, 256, 3, activation='relu')
+network = max_pool_2d(network, 3, strides=2)
+network = local_response_normalization(network)
+network = fully_connected(network, 4096, activation='tanh')
+network = dropout(network, 0.5)
+network = fully_connected(network, 4096, activation='tanh')
+network = dropout(network, 0.5)
+network = fully_connected(network, max_num, activation='softmax')
+network = regression(network, optimizer='momentum',
+                     loss='categorical_crossentropy',
+                     learning_rate=0.001)
+
+# Training
+model = tflearn.DNN(network, checkpoint_path='model_alexnet',
+                    max_checkpoints=1, tensorboard_verbose=2)
+model.fit(X, Y, n_epoch=1000, validation_set=0.1, shuffle=True,
+          show_metric=True, batch_size=64, snapshot_step=200,
+          snapshot_epoch=False, run_id='alexnet_counting_people')
